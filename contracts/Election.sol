88628a2fa8f394db6c9cd9a129edbecbacca8d5f
hossam-k <hossam.m.khalil@gmail.com>
2018-12-12 15:23:33 +0200

    cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -11,6 +11,9 @@ contract Election {
 		string name;
 		uint voteCount;		
 	}	
+	// store accounts that have voted
+	mapping (address => bool) public voters ;
+	
 	// Store Candidates
 	// Fetch Candidate
 	mapping (uint => Candidate) public candidates;
@@ -27,6 +30,19 @@ contract Election {
 		candidatesCount ++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
+
+	function vote (uint _candidateId) public {
+		//require that they haven't voted before
+		require (!voters[msg.sender]);
+		//require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+		//record that voter has voted
+		voters[msg.sender] = true;
+ 
+		// update candidate vote count
+		candidates[_candidateId].voteCount ++;
+	}
+	
 	
 }
 
